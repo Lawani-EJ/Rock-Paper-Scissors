1e7a08f6d5c3af408933dfd7415516b1809c2a34
Lawani-EJ <elyonjohn60@gmail.com>
2024-11-01 22:36:41 +0100

    Working on funcntionalites

---
+++ script.js
@@ -0,0 +1,14 @@
+// Defining some variables 
+let winMsg = 'Victory';
+let loseMsg = 'Crushing Defeat';
+let tieMsg = 'Tie';
+let moveList = ['Rock','Paper','Scissors'];
+
+// Functions
+
+function startGame(){
+// Displays the starting of the game 
+// And it will display input prompts 
+
+
+}
